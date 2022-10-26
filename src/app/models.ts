8ed948b2759c01221f1cbd958586e9c3d5a8a96e
Anish kumar Srinivaasan <anishkumar92@gmail.com>
2022-10-26 16:34:14 +0530

    Added interface model to declare dataTypes

---
+++ src/app/models.ts
@@ -0,0 +1,68 @@
+    export interface DaCurrency {
+        currency: string;
+    }
+
+    export interface BaseCurrency {
+        currency: string;
+        exchangeRate: number;
+    }
+
+    export interface CostItemAlias {
+        accountingCode: string;
+    }
+
+    export interface Annotation {
+        id: number;
+        name: string;
+    }
+
+    export interface CostItemCost {
+        daStage: string;
+        persona: string;
+        type: string;
+        amount: number;
+    }
+
+    export interface Comment {
+        id: number;
+        daStage: string;
+        persona: string;
+        author: string;
+        comment: string;
+        type: string;
+        date: Date;
+    }
+
+    export interface CostItem {
+        id: number;
+        name: string;
+        costItemAlias: CostItemAlias;
+        annotation: Annotation;
+        costs: CostItemCost[];
+        comments: Comment[];
+    }
+
+    export interface Cost {
+        id: number;
+        name: string;
+        displayOrder: number;
+        costItems: CostItem[];
+    }
+
+    export interface CostAPI {
+        daCurrency: DaCurrency;
+        baseCurrency: BaseCurrency;
+        costs: Cost[];
+    }
+
+
+    export interface PaymentCurrency {
+        fromCurrency: string;
+        toCurrency: string;
+        exchangeRate: number;
+    }
+
+    export interface ExchangeRateAPI {
+        sourceCurrency: string;
+        paymentCurrencies: PaymentCurrency[];
+    }
